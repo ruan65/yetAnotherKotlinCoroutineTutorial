8f0034f3ab6782da5ff77666e24ea25f9ab97577
a <ruan65@gmail.com>
2018-10-21 23:31:04 +0300

    async_await

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,7 +4,7 @@ import java.util.concurrent.Executors
 
 fun main(args: Array<String>) {
 
-    exampleLaunchCoroutineScope()
+    exampleAsyncAwait()
 }
 
 suspend fun printDelayed(message: String) {
@@ -61,4 +61,25 @@ fun exampleLaunchCoroutineScope() = runBlocking {
     println("three - from thread ${Thread.currentThread().name}")
 
     (customDispatcher.executor as ExecutorService).shutdown()
+}
+
+suspend fun calculateHardThings(number: Int) : Int {
+    delay(1000)
+    return number * 10
+}
+
+fun exampleAsyncAwait() = runBlocking {
+
+    val start = System.currentTimeMillis()
+
+    val deferred1 = async { calculateHardThings(10) }
+    val deferred2 = async { calculateHardThings(20) }
+    val deferred3 = async { calculateHardThings(30) }
+
+    val sum = deferred1.await() + deferred2.await() + deferred3.await()
+
+    println("sum = $sum")
+
+    println("Time taken: ${System.currentTimeMillis() - start}")
+
 }
